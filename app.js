5c0bcc3b1cf8bb6967751248498ef0a65a8b283d
MinnyNatchaya <mintnumber2@gmail.com>
2021-11-14 00:23:26 +0700

    socket reload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express(); //dddddd
+const app = express();
 
 const cors = require('cors');
 
@@ -34,10 +34,101 @@ app.use('/category', categoryRoute);
 app.use('/city', cityRoute);
 app.use('/', authRoute);
 
+////////////////SOCKET//////////////////////////////////////////
+const http = require('http');
+const { Server } = require('socket.io');
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:3000`,
+    methods: ['GET', 'POST']
+  }
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  if (!users.some(item => +item.userId === +userId)) {
+    users.push({ userId, socketId });
+  }
+};
+
+const removeUser = socketId => {
+  users = users.filter(item => item.socketId !== socketId);
+};
+
+const findUserIdSocket = userId => {
+  return users.filter(item => item.userId === userId); // [{a,b}]
+};
+
+io.on('connection', socket => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('addSocketUser', userId => {
+    // console.log(userId);
+    addUser(userId, socket.id);
+    console.log('connectionaddUser', users);
+  });
+
+  socket.on('workerAcceptWork', data => {
+    // console.log(data);
+    if (findUserIdSocket(data.clientId).length !== 0) {
+      io.to(findUserIdSocket(data.clientId)[0].socketId).emit('receive_workerAcceptWork');
+    }
+  });
+
+  socket.on('workerCancleWork', data => {
+    // console.log(data);
+    if (findUserIdSocket(data.clientId).length !== 0) {
+      io.to(findUserIdSocket(data.clientId)[0].socketId).emit('receive_workerCancleWork');
+    }
+  });
+
+  socket.on('workerCancleSlip', data => {
+    // console.log(data);
+    if (findUserIdSocket(data.clientId).length !== 0) {
+      io.to(findUserIdSocket(data.clientId)[0].socketId).emit('receive_workerCancleSlip');
+    }
+  });
+
+  socket.on('workerAcceptSlip', data => {
+    // console.log(data);
+    if (findUserIdSocket(data.clientId).length !== 0) {
+      io.to(findUserIdSocket(data.clientId)[0].socketId).emit('receive_workerAcceptSlip');
+    }
+  });
+
+  socket.on('changeWorker', data => {
+    // console.log(data);
+    if (findUserIdSocket(data.workerId).length !== 0) {
+      io.to(findUserIdSocket(data.workerId)[0].socketId).emit('receive_changeWorker');
+    }
+  });
+
+  socket.on('clientUploadSlip', data => {
+    // console.log(data);
+    if (findUserIdSocket(data.workerId).length !== 0) {
+      io.to(findUserIdSocket(data.workerId)[0].socketId).emit('receive_clientUploadSlip');
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
+    removeUser(socket.id);
+    console.log('disconnectRemoneUser', users);
+  });
+});
+
+//////////////////////////////////////////////////////////
+
 app.use((req, res, next) => {
   res.status(404).json({ message: 'resource not found on this server' });
 });
 
 app.use(errorController);
 const port = process.env.PORT || 8099;
-app.listen(port, () => console.log(`server running on port ${port}`));
+
+server.listen(port, () => console.log(`server running on port ${port}`));
